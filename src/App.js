1de157c6fc273409f236a89b1d0f59ca85436d75
aman-janwani <amanjanwani1486@gmail.com>
2021-09-16 11:53:42 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const colors = [
+    "red",
+    "green",
+    "red",
+    "blue",
+    "purple",
+    "yellow",
+    "brown",
+    "purple",
+    "aqua",
+    "black",
+  ];
+  const [term, setTerm] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>A-J Color Search</h1>
+      <input
+        type="text"
+        placeholder="Enter color name"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+      />
+      <h1>{term}</h1>
+      <div className="colors">
+        {colors
+          .filter((color) => color.includes(term))
+          .map((color, index) => {
+            console.log(color);
+            return (
+              <div
+                className="colors__single"
+                style={{ backgroundColor: color }}
+                key={index}
+              ></div>
+            );
+          })}
+      </div>
     </div>
   );
 }
